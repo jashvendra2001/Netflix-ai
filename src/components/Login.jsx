0860d165a32ff126ce62cbce6fdc3eb4ad0bc6c3
jashvendra <jschauhan2001@gmail.com>
2024-05-07 16:43:06 +0530

    log in signup complete some bugs are pending currently working on it

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,65 +1,93 @@
-import React, { useState ,useRef} from 'react'
-import Header from './Header'
-import {validateFields} from "../utility/validation"
-
+import React, { useState, useRef } from 'react';
+import Header from './Header';
+import { validateFields } from "../utility/validation";
+import { getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
 
 const Login = () => {
-  const[isNewUser,setNewUser]=useState(true);
-  const[errorMessage,setErrorMessage]=useState("")
+  const [isNewUser, setNewUser] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
 
+  const email = useRef(null);
+  const password = useRef(null);
 
-  const email=useRef(null);
-  const password=useRef(null)
-
-  const handleSignup=()=>{
+  const handleSignup = () => {
     setNewUser(!isNewUser);
-
-  }
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     // Submit the form data here
-
   };
 
-  const handleButtonClick=()=>{
-    console.log(email.current.value)
-    console.log(password.current.value)
+  const handleButtonClick = () => {
+    const emailValue = email.current.value;
+    const passwordValue = password.current.value;
 
-   const error= validateFields(email.current.value,password.current.value)
-   console.log()
-   setErrorMessage(error)
-  }
+    const error = validateFields(emailValue, passwordValue);
+    setErrorMessage(error);
+
+    if (error) return;
 
   
-  
+    if (!isNewUser) {
+      const auth = getAuth();
+       createUserWithEmailAndPassword(auth, emailValue, passwordValue)
+        .then((userCredential) => {
+          // Signed up
+          const user = userCredential.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(`Allready Signup `);
+        });
+    } 
+    else {
+      // Logic for handling sign in
+      const auth = getAuth();
+signInWithEmailAndPassword(auth, emailValue, passwordValue)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log(user)
+    alert("login Successfully")
+
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    setErrorMessage(`wrong login and Password `);
+
+
+  });
+    }
+  };
+
   return (
     <div>
-        <Header/>
-        <div className='absolute    '>
-            <img className='z-1' src="https://assets.nflxext.com/ffe/siteui/vlv3/4d7bb476-6d8b-4c49-a8c3-7739fddd135c/deecf71d-7a47-4739-9e1a-31b6b0d55be7/IN-en-20240429-popsignuptwoweeks-perspective_alpha_website_medium.jpg" alt="" />
-        </div>
+      <Header />
+      <div className='absolute'>
+        <img className='z-1 h-[100vh] object-cover' src="https://assets.nflxext.com/ffe/siteui/vlv3/4d7bb476-6d8b-4c49-a8c3-7739fddd135c/deecf71d-7a47-4739-9e1a-31b6b0d55be7/IN-en-20240429-popsignuptwoweeks-perspective_alpha_website_medium.jpg" alt="" />
+      </div>
 
-        <div className="form flex justify-center ">
-        <form onSubmit={handleSubmit} className=' absolute p-12 bg-black  my-36 mx-auto  flex flex-col  bg-opacity-70'>
-            <h1 className='text-bold-3xl py-4 text-white font-bold text-2xl'>{isNewUser?"Log In":"SignUp"}</h1>
-          { !isNewUser && <input type="text" placeholder='UserName' className='p-2 m-2 bg-gray-600 rounded-lg' />}
+      <div className="form flex justify-center">
+        <form onSubmit={handleSubmit} className='absolute p-12 bg-black  my-36 mx-auto  flex flex-col  bg-opacity-70'>
+          <h1 className='text-bold-3xl py-4 text-white font-bold text-2xl'>{isNewUser ? "Log In" : "Sign Up"}</h1>
+          {!isNewUser && <input type="text" placeholder='UserName' className='p-3 m-2 bg-gray-600 rounded-lg' autoComplete='false'/>}
 
-            <input ref={email} type="text" placeholder='Email' className='p-2 m-2 bg-gray-600 rounded-lg' />
-            <p className='text-[red] pl-3 '>{errorMessage==="Email Id is not correct " ?"Email Id is not correct":""}</p>
-            <input ref={password} type="password" placeholder='Password' className='p-2 m-2 bg-gray-600 rounded-lg' />
-            <p className='text-[red] pl-3 '>{errorMessage==="Password is not correct" ?"Password is not correct":""}</p>
+          <input ref={email} type="text" placeholder='Email' className='p-3 m-2 bg-gray-600 rounded-lg text-white' autoCorrect='false' />
+          
+          <input ref={password} type="password" placeholder='Password' className='p-3 m-2 bg-gray-600 rounded-lg text-white' />
+          {errorMessage && <p className='text-red-500 pl-3'>{errorMessage}</p>}
 
-            <button onClick={handleButtonClick} className='p-2 m-2  text-white rounded-lg' style={{background:"rgb(193, 17, 25)"}}>{isNewUser?"Log In":"Sign Up "}</button>
-            <p className='text-white'>New to Netflix ? <button onClick={handleSignup}>{isNewUser?"Sign Up Now":"Log In"}</button></p>
-
+          <button onClick={handleButtonClick} className='p-2 m-2  text-white rounded-lg' style={{ background: "rgb(193, 17, 25)" }}>{isNewUser ? "Log In" : "Sign Up"}</button>
+          <p className='text-white'>New to Netflix? <button onClick={handleSignup}>{isNewUser ? "Sign Up Now" : "Log In"}</button></p>
         </form>
-
-        </div>
-        
-        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC5IasDOrb4YDs3ObK22zRT33Cntgep0HM",
+  authDomain: "crud-9a8fb.firebaseapp.com",
+  databaseURL: "https://crud-9a8fb-default-rtdb.firebaseio.com",
+  projectId: "crud-9a8fb",
+  storageBucket: "crud-9a8fb.appspot.com",
+  messagingSenderId: "582801962599",
+  appId: "1:582801962599:web:4cf623ce26712d16a646c2",
+  measurementId: "G-G4F5GLWXE8"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
